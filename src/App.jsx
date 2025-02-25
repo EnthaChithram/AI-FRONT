66ba5b553b4041dc2a332adfc2821d186ed2f098
EnthaChithram <ambatisuraj5@gmail.com>
2025-02-25 18:19:52 +0530

    added server status

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,29 @@ function App() {
   const [inputValue, setInputValue] = useState('')
   const [color, setColor] = useState("black")
   const [loading, setLoading] = useState(false)
+
+
+  const [serverStatus, setServerStatus] = useState(2)
+
+  const checkServerStatus = async () => {
+    setServerStatus(2)
+    const response = await fetch('https://aiaiai-zfg6.onrender.com/check')
+    if (response.ok) {
+      setServerStatus(1)
+    } else {
+      setServerStatus(0)
+    }
+  }
+
+  useEffect(() => {
+    checkServerStatus()
+  }, [])
+
+
   const handleSubmit = async (e) => {
     e.preventDefault()
     setLoading(true)
+    checkServerStatus()
 
     const response = await fetch('https://aiaiai-zfg6.onrender.com/', {
       method: 'POST',
@@ -29,33 +49,33 @@ function App() {
     }
     setLoading(false)
 
-    const check = await fetch('https://aiaiai-zfg6.onrender.com/check')
 
-
-    console.log(check)
   }
 
 
 
 
   return (
-    <div style={{ backgroundColor: color }} className="trasition-all duration-500 text-white flex flex-col items-center justify-center h-screen">
-      <form className=" flex flex-col  items-center justify-center  space-y-4" onSubmit={handleSubmit}>
+    <div style={{ backgroundColor: color }} className="trasition-all duration-500 text-white flex flex-col space-y-2 items-center justify-center h-screen">
+
+      <div className='mb-10'><strong>server status: </strong><span>{serverStatus === 0 ? "server down" : serverStatus === 1 ? "running" : "starting server... (may take upto 30 seconds)"}</span></div>
+      <br></br>
+      <form className="flex flex-col  items-center justify-center  space-y-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Enter a word"
-          className="border bg-black rounded-lg p-2 focus:outline-none "
+          className="border bg-black rounded-3xl p-2  focus:outline-none text-center "
           required
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
-        <button type="submit" className="text-black bg-white  px-4 py-2 rounded-lg cursor-pointer">{loading ? "Loading..." : "Submit"}</button>
+        <button type="submit" className="text-black bg-white  px-4 py-2 rounded-3xl cursor-pointer">{loading ? "Loading..." : "Submit"}</button>
       </form>
 
       <p>{color}</p>
 
-      <p className='mt-10 text-center'>The answers are from an LLM called "mistral". Its a free API and the output might be inconsistent.</p>
-      <p className='mt-5'>Hit submit again if it keeps loading</p>
+      <strong className='mt-10 text-center'>Uses MistralAI through OpenRouter API. Output might be inconsistent.</strong>
+      <strong className='mt-5'>Hit submit again if it keeps loading</strong>
 
     </div>
 
